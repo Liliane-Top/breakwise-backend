90fb7a7f961777b10de1ada0b4e1aa348fd881e5
Liliane Top <liliane.top@uwv.nl>
2025-04-11 11:33:15 +0200

    initial commit

---
+++ src/main/java/nl/lilianetop/breakwize/BreakWizeApplication.java
@@ -0,0 +1,13 @@
+package nl.lilianetop.breakwize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakWizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakWizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/lilianetop/breakwize/BreakWizeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.lilianetop.breakwize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakWizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
